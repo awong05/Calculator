f25e1112e86316bdf9d5658a97a1e8f85e199dbf
Andy Wong <awong@propelmarketing.com>
2015-06-28 17:15:50 -0400

    Initial model for CalculatorBrain.

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,41 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Andy Wong on 6/28/15.
+//  Copyright (c) 2015 Propel Marketing. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain
+{
+    enum Op {
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+    }
+    
+    var opStack = [Op]()
+    
+    var knownOps = [String:Op]()
+    
+    init() {
+        knownOps["×"] = Op.BinaryOperation("×", *)
+        knownOps["÷"] = Op.BinaryOperation("×") { $1 / $0 }
+        knownOps["+"] = Op.BinaryOperation("+", +)
+        knownOps["−"] = Op.BinaryOperation("−") { $1 - $0 }
+        knownOps["√"] = Op.UnaryOperation("√", sqrt)
+    }
+    
+    func pushOperand(operand: Double) {
+        opStack.append(Op.Operand(operand))
+    }
+    
+    func performOperation(symbol: String) {
+        if let operation = knownOps[symbol] {
+            opStack.append(operation)
+        }
+        
+    }
+}
