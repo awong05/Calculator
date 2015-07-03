aa9ac834f1b25a2b8fbc861b73c53b181cff3dab
Andy Wong <awong@propelmarketing.com>
2015-07-03 15:39:47 -0400

    Copying implementation for evaluate()

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -56,11 +56,43 @@ class CalculatorBrain
         knownOps["π"] = Op.Constant("π")
     }
     
+    // TODO: Currently returning only the latest result, not full string.
     private func description(ops: [Op]) -> (result: String, remainingOps: [Op])
     {
         if !ops.isEmpty {
             var remainingOps = ops
-            return (" ", remainingOps)
+            let op = remainingOps.removeLast()
+            switch op {
+            case .Operand(let operand):
+                return ("\(operand)", remainingOps)
+            case .Variable(let variable):
+                if let variableValue = variableValues[variable] {
+                    return ("\(variableValue)", remainingOps)
+                } else {
+                    return (" ", ops)
+                }
+            case .Constant(let constant):
+                switch constant {
+                case "π":
+                    let constantValue = M_PI
+                    return ("\(constantValue)", remainingOps)
+                default:
+                    return (" ", remainingOps)
+                }
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result {
+                    return ("\(operation(operand))", operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remainingOps)
+                if let operand1 = op1Evaluation.result {
+                    let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    if let operand2 = op2Evaluation.result {
+                        return ("\(operation(operand1, operand2))", op2Evaluation.remainingOps)
+                    }
+                }
+            }
         }
         return (" ", ops)
     }
